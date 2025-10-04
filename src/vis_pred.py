3e9ed5dd6a94e3398674919c1abddd8fc584b039
yao_scl <maxmax0323@gmail.com>
2025-10-04 20:40:12 +0800

    update

--- src/vis_pred.py
+++ src/vis_pred.py
@@ -24,7 +24,7 @@ import torchvision
 from model import get_fasterrcnn_r50_fpn
 
 # ========== 可自行修改的設定 ==========
-IMG_DIR     = "data/mini_teat"                      # 要可視化的影像資料夾（小測試集/驗證集/任一資料夾）
+IMG_DIR     = "data/mini_test"                      # 要可視化的影像資料夾（小測試集/驗證集/任一資料夾）
 CKPT_PATH   = "experiments/logs/fasterrcnn_r50fpn_final_v1.pth"  # 權重
 OUT_DIR     = "experiments/vis_pred"              # 輸出的圖片資料夾
 MAX_SIDE    = 800                                  # 推論時最長邊縮放（保持等比）
